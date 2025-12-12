1afcf4ba2935f75477473567a2ab771c9e48928d
kravchuk-st <qkp@ukr.net>
2025-12-13 00:52:54 +0200

    feat: add adaptive

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,6 +1,44 @@
+const body = document.body;
 const burger = document.querySelector(".burger");
+const menu = document.querySelector(".nav");
+const overlay = document.querySelector(".overlay");
+
+const openMenu = () => {
+  let paddingOffset = window.innerWidth - document.body.offsetWidth + "px";
+
+  menu.classList.add("open");
+  burger.classList.add("burger_open");
+  burger.setAttribute("aria-expanded", "true");
+  burger.setAttribute("aria-label", "Закрити меню");
+  body.style.paddingRight = paddingOffset;
+  body.classList.add("disable-scroll");
+};
+
+const closeMenu = () => {
+  menu.classList.remove("open");
+  burger.classList.remove("burger_open");
+  burger.setAttribute("aria-expanded", "false");
+  burger.setAttribute("aria-label", "Відкрити меню");
+  body.style = null;
+  body.classList.remove("disable-scroll");
+};
 
 burger.addEventListener("click", () => {
-  let paddingOffset = window.innerWidth - document.body.offsetWidth + "px";
-  burger.classList.toggle("burger_open");
+  if (menu.classList.contains("open")) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+});
+
+overlay.addEventListener("click", () => {
+  if (menu.classList.contains("open")) {
+    closeMenu();
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && menu.classList.contains("open")) {
+    closeMenu();
+  }
 });
--- assets/js/slider.js
+++ assets/js/slider.js
@@ -2,12 +2,31 @@ new Splide(".slider", {
   perPage: 3,
   perMove: 1,
   type: "loop",
-  // fixedWidth: 590,
   autoplay: true,
   pauseOnHover: true,
   pagination: false,
-  // padding: { left: 300, right: 300 },
   gap: 40,
   flickMaxPages: 1,
-  breakpoints: {},
+  breakpoints: {
+    1500: {
+      perPage: 2,
+      padding: { left: 100, right: 100 },
+    },
+    1280: {
+      padding: { left: 0, right: 0 },
+    },
+    992: {
+      perPage: 1,
+      padding: { left: 100, right: 100 },
+      gap: 30,
+    },
+    768: {
+      padding: { left: 50, right: 50 },
+      gap: 50,
+    },
+    576: {
+      padding: { left: 40, right: 40 },
+      gap: 15,
+    },
+  },
 }).mount();
